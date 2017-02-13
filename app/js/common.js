773092fe78bf8701ed3f4838173e176bfaa70ec9
megaluc <andrej.lutsenko@gmail.com>
2017-02-13 13:48:26 +0200

    lck3

--- app/js/common.js
+++ app/js/common.js
@@ -2,10 +2,16 @@ $(function() {
 
     $("#sticker").sticky({topSpacing:0});
 
+    $('.analytics-left__content').hover(function(){
+        $('.first-list').addClass("active");
+    },function(){
+        $('.menus-lists').removeClass("active");
+    });
+
     $('.rol').hover(function(){
         $('.second-list').addClass("active");
     },function(){
-        $(this).prev().removeClass("active");
+        $('.menus-lists').removeClass("active");
     });
 
 });
