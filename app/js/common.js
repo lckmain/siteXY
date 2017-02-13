9fb04f078069388a6c4161681cf9532250b0c55d
megaluc <andrej.lutsenko@gmail.com>
2017-02-13 13:27:15 +0200

    lck

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,10 @@ $(function() {
 
     $("#sticker").sticky({topSpacing:0});
 
+    $('.rol').hover(function(){
+        $('.second-list').addClass("active");
+    },function(){
+        $(this).prev().removeClass("active");
+    });
+
 });
