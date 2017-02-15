86d862712771f3693dac6a071715b04dfb634e9d
megaluc <andrej.lutsenko@gmail.com>
2017-02-15 10:25:37 +0200

    last block

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,8 @@
 $(function() {
 
-    $("#sticker").sticky({topSpacing:0});
+	$("#sticker").sticky({topSpacing:0});
 
-    $('.analytics-left__content').hover(function(){
+    $('.analytics').hover(function(){
         $('.first-list').addClass("active");
     },function(){
         $('.menus-lists').removeClass("active");
@@ -14,4 +14,34 @@ $(function() {
         $('.menus-lists').removeClass("active");
     });
 
+		$('.prodvigenie-wrap').hover(function(){
+        $('.six-list').addClass("active");
+    },function(){
+        $('.menus-lists').removeClass("active");
+    });
+
+		//scroll
+		function getTargetTop(a) {
+		    var c = a.attr("href");
+		    var b = 0;
+		    return $(c).offset().top - b
+		}
+		$('.menus-lists[href^="#"]').click(function (a) {
+		    var b = getTargetTop($(this));
+		    $("html, body").animate({scrollTop: b}, 1500);
+		    a.preventDefault()
+		});
+		var sections = $('.menus-lists[href^="#"]');
+		function checkSectionSelected(c) {
+		    var a = 100;
+		    var b;
+		    for (b = 0; b < sections.length; b++) {
+		        var e = $(sections[b]);
+		        var d = getTargetTop(e);
+		        if (c > d - a && c < d + a) {
+		            sections.removeClass("active");
+		            e.addClass("active")
+		        }
+		    }
+		}
 });
